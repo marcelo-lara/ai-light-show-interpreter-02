5cb98776f549d11f559a5111d8cadcc5544f1dc2
Marcelo Lara <marcelo.lara@globant.com>
2026-05-01 16:47:00 -0300

    allow s2 host

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["s2.local"],
+    host: "0.0.0.0",
+    port: 3300,
+  },
+  preview: {
+    allowedHosts: ["s2.local"],
     host: "0.0.0.0",
     port: 3300,
   },
